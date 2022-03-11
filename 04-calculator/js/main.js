5cfe07ae378a9f4579195e19b5815c59b79cf6b7
atia009 <aarontia009@gmail.com>
2022-03-11 13:15:55 -0800

    Created js directory with main.js.  Created functions for add, subtract, multiply, and divide.

---
+++ 04-calculator/js/main.js
@@ -0,0 +1,23 @@
+
+// functions
+function startAdd(num1, num2) {
+    return num1 + num2;
+}
+
+function startSubtract(num1, num2) {
+    return num1 - num2;
+}
+
+function startMultiply(num1, num2) {
+    return num1 * num2;
+}
+
+function startDivide(num1, num2) {
+    return num1 / num2;
+}
+
+// test
+console.log(startAdd(4, 2));
+console.log(startSubtract(4, 2));
+console.log(startMultiply(4, 2));
+console.log(startDivide(4, 2));
