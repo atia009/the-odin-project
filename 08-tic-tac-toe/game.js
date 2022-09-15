63e813637d4df6456d683ca99e91ccf8070d457d
atia009 <aarontia09@gmail.com>
2022-09-15 08:51:48 -0700

    Add functionality for mark to appear when user clicks a square

--- 08-tic-tac-toe/game.js
+++ 08-tic-tac-toe/game.js
@@ -1,9 +1,13 @@
 const gameBoard = (function(){
   const board = [``, ``, ``, ``, ``, ``, ``, ``, ``];
+  let mark = `X`;
 
   // cache DOM
   const el = document.querySelector(`.gameboard-module`);
   const template = el.querySelector(`.gameboard-template`).innerHTML;
+
+  // bind events
+  el.addEventListener(`click`, addMark);
 
   render();
   // functions
@@ -13,5 +17,18 @@ const gameBoard = (function(){
       templateHTML += template.replace(/{{.}}/g, square);
     });
     el.innerHTML = templateHTML;
-  }
+  };
+
+  function addMark(event) {
+    const index = Array.from(document.querySelectorAll(`.square`)).indexOf(event.target.closest(`div`));
+    board.splice(index, 1, mark);
+    updateMark();
+    render();
+  };
+  
+  function updateMark() {
+    mark = (mark === `X`) ? `O` : `X`;
+  };
+
 })();
+
